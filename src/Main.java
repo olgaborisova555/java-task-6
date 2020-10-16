3a01df7d97b1e57423a9022724f4129cd2c0b8c5
Nadezda Vasilyeva <nadezda.vasilyeva@gmail.com>
2020-10-16 18:15:26 +0300

    correction

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int popolnenie = 1500;
+        int popolnenie = 500;
         int present = 100;
+        int bonus;
 
-        int bonus = popolnenie / present;
-        System.out.println("Ваш баланс " + bonus +  " бонуса");
-
-        popolnenie = 500;
-        bonus = 0;
-        System.out.println("Ваш баланс " + bonus +  " бонуса");
+        if (popolnenie > 1000){
+            bonus = popolnenie / present;
+            System.out.println("Ваш баланс " + bonus +  " бонуса");
+        } else {
+            bonus = 0;
+            System.out.println("Ваш баланс " + bonus +  " бонуса");
+        }
     }
 }
